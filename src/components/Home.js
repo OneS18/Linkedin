5134a0c5a58e423629a3486b25a95868d7be7c0d
OneS18 <diablo201888@gmail.com>
2021-09-15 22:48:30 +0300

    finish left and right side ,need to do main component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
+import Leftside from './Leftside';
+import Main from './Main';
+import Rightside from './Rightside';
 
 const Home = (props) => {
     return (
-        <div>
-            home
-        </div>
+        <Container>
+            <Layout>
+                <Leftside />
+                <Main />
+                <Rightside />
+            </Layout>
+        </Container>
     );
 };
+
+const Container = styled.div`
+    padding-top: 60px;
+    max-width:100%;
+`;
+
+const Layout = styled.div`
+    display: grid;
+    grid-template-areas:'leftside main righthside';
+    grid-template-columns: minmax(0, 5fr)minmax(0,12fr)minmax(300px, 7fr);
+    columns-gap:25px;
+    row-gap: 25px;
+    /* grid-template-row: auto; */
+    margin:25px 0;
+
+    @media (max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        padding:0 5px;
+    }
+`;
+
 
 export default Home;
