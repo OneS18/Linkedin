8b3049862452c035c02ccd4a711645ff587df62c
OneS18 <diablo201888@gmail.com>
2021-09-15 14:24:49 +0300

    finish the '/' , need to finish the header

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Home = (props) => {
+    return (
+        <div>
+            home
+        </div>
+    );
+};
+
+export default Home;
